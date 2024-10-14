e179478b0f6ba6ccf26746344e7a7453d6346efd
akvlad <akvlad90@gmail.com>
2024-10-14 16:42:20 +0300

    some types support

--- src/httpserver_extension.cpp
+++ src/httpserver_extension.cpp
@@ -36,18 +36,52 @@ struct HttpServerState {
 
 static HttpServerState global_state;
 
+    std::string GetColumnType(MaterializedQueryResult &result, idx_t column) {
+        if (result.RowCount() == 0) {
+            return "String";
+        }
+        switch (result.types[column].id()) {
+            case LogicalTypeId::FLOAT:
+                return "Float";
+            case LogicalTypeId::DOUBLE:
+                return "Double";
+            case LogicalTypeId::INTEGER:
+                return "Int32";
+            case LogicalTypeId::BIGINT:
+                return "Int64";
+            case LogicalTypeId::UINTEGER:
+                return "UInt32";
+            case LogicalTypeId::UBIGINT:
+                return "UInt64";
+            case LogicalTypeId::VARCHAR:
+                return "String";
+            case LogicalTypeId::TIME:
+                return "DateTime";
+            case LogicalTypeId::DATE:
+                return "Date";
+            case LogicalTypeId::TIMESTAMP:
+                return "DateTime";
+            case LogicalTypeId::BOOLEAN:
+                return "Int8";
+            default:
+                return "String";
+        }
+        return "String";
+    }
+
 // Convert the query result to JSON format
 static std::string ConvertResultToJSON(MaterializedQueryResult &result) {
     auto doc = yyjson_mut_doc_new(nullptr);
     auto root = yyjson_mut_obj(doc);
     yyjson_mut_doc_set_root(doc, root);
-
     // Add meta information
     auto meta_array = yyjson_mut_arr(doc);
     for (idx_t col = 0; col < result.ColumnCount(); ++col) {
         auto column_obj = yyjson_mut_obj(doc);
         yyjson_mut_obj_add_str(doc, column_obj, "name", result.ColumnName(col).c_str());
         yyjson_mut_arr_append(meta_array, column_obj);
+        std::string tp(GetColumnType(result, col));
+        yyjson_mut_obj_add_strcpy(doc, column_obj, "type", tp.c_str());
     }
     yyjson_mut_obj_add_val(doc, root, "meta", meta_array);
 
@@ -70,7 +104,11 @@ static std::string ConvertResultToJSON(MaterializedQueryResult &result) {
 
     // Add row count
     yyjson_mut_obj_add_int(doc, root, "rows", result.RowCount());
-
+    //"statistics":{"elapsed":0.00031403,"rows_read":1,"bytes_read":0}}
+    auto stat_obj = yyjson_mut_obj_add_obj(doc, root, "statistics");
+    yyjson_mut_obj_add_real(doc, stat_obj, "elapsed", 0.01);
+    yyjson_mut_obj_add_int(doc, stat_obj, "rows_read", 1);
+    yyjson_mut_obj_add_int(doc, stat_obj, "bytes_read", 0);
     // Write to string
     auto data = yyjson_mut_write(doc, 0, nullptr);
     if (!data) {
