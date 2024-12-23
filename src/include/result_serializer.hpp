f10d81a2af9b9a95253512c100bc0b2bb315b9e5
Niclas Haderer <niclas.haderer.dev@gmail.com>
2024-12-23 14:52:26 +0100

    Moved up clang-format and tidy config so IDE and make format picks up on the right formatting rules

--- src/include/result_serializer.hpp
+++ src/include/result_serializer.hpp
@@ -9,46 +9,45 @@ namespace duckdb {
 
 class ResultSerializer {
 public:
-  explicit ResultSerializer(const bool _set_invalid_values_to_null = false)
-      : set_invalid_values_to_null(_set_invalid_values_to_null) {
-    doc = yyjson_mut_doc_new(nullptr);
-    root = yyjson_mut_arr(doc);
-    if (!root) {
-      throw SerializationException("Could not create yyjson array");
-    }
-    yyjson_mut_doc_set_root(doc, root);
-  }
-
-  ~ResultSerializer() { yyjson_mut_doc_free(doc); }
-
-  void SerializeChunk(const DataChunk &chunk, vector<string> &names,
-                      vector<LogicalType> &types, bool values_as_array);
-
-  yyjson_mut_val *Serialize(QueryResult &query_result, bool values_as_array);
-
-  yyjson_mut_val *SerializeRowAsArray(const DataChunk &chunk, idx_t row_idx,
-                                      vector<LogicalType> &types);
-
-  yyjson_mut_val *SerializeRowAsObject(const DataChunk &chunk, idx_t row_idx,
-                                       vector<string> &names,
-                                       vector<LogicalType> &types);
-
-  static std::string YY_ToString(yyjson_mut_doc *val) {
-    auto data = yyjson_mut_write(val, 0, nullptr);
-    if (!data) {
-	  throw SerializationException("Could not render yyjson document");
+	explicit ResultSerializer(const bool _set_invalid_values_to_null = false)
+	    : set_invalid_values_to_null(_set_invalid_values_to_null) {
+		doc = yyjson_mut_doc_new(nullptr);
+		root = yyjson_mut_arr(doc);
+		if (!root) {
+			throw SerializationException("Could not create yyjson array");
+		}
+		yyjson_mut_doc_set_root(doc, root);
 	}
-    std::string json_output(data);
-    free(data);
-    return json_output;
-  }
+
+	~ResultSerializer() {
+		yyjson_mut_doc_free(doc);
+	}
+
+	void SerializeChunk(const DataChunk &chunk, vector<string> &names, vector<LogicalType> &types,
+	                    bool values_as_array);
+
+	yyjson_mut_val *Serialize(QueryResult &query_result, bool values_as_array);
+
+	yyjson_mut_val *SerializeRowAsArray(const DataChunk &chunk, idx_t row_idx, vector<LogicalType> &types);
+
+	yyjson_mut_val *SerializeRowAsObject(const DataChunk &chunk, idx_t row_idx, vector<string> &names,
+	                                     vector<LogicalType> &types);
+
+	static std::string YY_ToString(yyjson_mut_doc *val) {
+		auto data = yyjson_mut_write(val, 0, nullptr);
+		if (!data) {
+			throw SerializationException("Could not render yyjson document");
+		}
+		std::string json_output(data);
+		free(data);
+		return json_output;
+	}
 
 private:
-  void SerializeValue(yyjson_mut_val *parent, const Value &value,
-                      optional_ptr<string> name, const LogicalType &type);
+	void SerializeValue(yyjson_mut_val *parent, const Value &value, optional_ptr<string> name, const LogicalType &type);
 
-  yyjson_mut_doc *doc;
-  yyjson_mut_val *root;
-  bool set_invalid_values_to_null;
+	yyjson_mut_doc *doc;
+	yyjson_mut_val *root;
+	bool set_invalid_values_to_null;
 };
 } // namespace duckdb
