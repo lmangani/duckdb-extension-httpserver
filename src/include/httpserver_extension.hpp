0bbeea4e2971e773f5541433eb5e8ab5ada4d64b
Niclas Haderer <niclas.haderer.dev@gmail.com>
2024-12-23 16:08:23 +0100

    Applied universal formatting

--- src/include/httpserver_extension.hpp
+++ src/include/httpserver_extension.hpp
@@ -9,12 +9,12 @@ class HttpserverExtension : public Extension {
 public:
 	void Load(DuckDB &db) override;
 	std::string Name() override;
-        std::string Version() const override;
+	std::string Version() const override;
 };
 
 // Static server state declarations
 struct HttpServerState;
-void HttpServerStart(DatabaseInstance& db, string_t host, int32_t port);
+void HttpServerStart(DatabaseInstance &db, string_t host, int32_t port);
 void HttpServerStop();
 
 } // namespace duckdb
